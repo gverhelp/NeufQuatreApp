f148aa503251dfeb16381ba26c2e1841430258ce
Garreth <garreth.verhelpen@gmail.com>
2025-03-17 16:15:34 +0100

    Starting app

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,11 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from neuf_quatre.api.urls import post_router
 from django.urls import path, include
 
 router = DefaultRouter()
-
-#neuf_quatre
-router.registry.extend(post_router.registry)
 
 urlpatterns = [
     path('', include(router.urls)),
