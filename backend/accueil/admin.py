deec5651d46deb3db57e733b3c341c9e2e2460ef
Garreth <garreth.verhelpen@gmail.com>
2025-04-26 20:35:37 +0200

    small changes

--- backend/accueil/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import AccueilItem
-
-# Register your models here.
-
-admin.site.site_header = "94 Admin"
-admin.site.site_title = "94 Admin Portal"
-admin.site.index_title = "Welcome to 94 Administration"
-admin.site.register(AccueilItem)
--- backend/accueil/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models import AccueilItem
-
-class AccueilItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = AccueilItem
-        fields = '__all__'
