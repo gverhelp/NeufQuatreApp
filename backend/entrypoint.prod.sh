a53dbf937237934c390749daab6a71b8f0c438c5
Garreth <garreth.verhelpen@gmail.com>
2025-05-03 15:56:18 +0200

    entrypoint.prod.sh no long create a superuser if one already exist

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -6,12 +6,20 @@ pipenv run python manage.py collectstatic --noinput
 # Créer le superutilisateur si les variables d'environnement sont définies
 if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]
 then
-    echo "Creating superuser..."
-    pipenv run python manage.py createsuperuser --noinput \
-    --username $DJANGO_SUPERUSER_USERNAME \
-    --email $DJANGO_SUPERUSER_EMAIL
+    echo "Checking if superuser already exists..."
+    SUPERUSER_EXISTS=$(pipenv run python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists())")
 
-    echo "from django.contrib.auth.models import User; user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | pipenv run python manage.py shell
+    if [ "$SUPERUSER_EXISTS" = "True" ]
+    then
+        echo "Superuser already exists. Skipping creation."
+    else
+        echo "Creating superuser..."
+        pipenv run python manage.py createsuperuser --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+
+        echo "from django.contrib.auth.models import User; user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | pipenv run python manage.py shell
+    fi
 else
     echo "Superuser not created because required environment variables are not set."
 fi
