c6a3b8ebb981409c157b861672a895e9acbaa1e3
Garreth <garreth.verhelpen@gmail.com>
2025-05-03 16:04:26 +0200

    entrypoint.prod.sh now recreate a superuser

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -4,17 +4,17 @@ pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic --noinput
 
 # Créer le superutilisateur si les variables d'environnement sont définies
-# if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]
-# then
-#     echo "Creating superuser..."
-#     pipenv run python manage.py createsuperuser --noinput \
-#     --username $DJANGO_SUPERUSER_USERNAME \
-#     --email $DJANGO_SUPERUSER_EMAIL
+if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_EMAIL" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]
+then
+    echo "Creating superuser..."
+    pipenv run python manage.py createsuperuser --noinput \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
 
-#     echo "from django.contrib.auth.models import User; user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | pipenv run python manage.py shell
-# else
-#     echo "Superuser not created because required environment variables are not set."
-# fi
+    echo "from django.contrib.auth.models import User; user = User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); user.set_password('$DJANGO_SUPERUSER_PASSWORD'); user.save()" | pipenv run python manage.py shell
+else
+    echo "Superuser not created because required environment variables are not set."
+fi
 
 # Exécuter la commande passée en argument
 exec "$@"
